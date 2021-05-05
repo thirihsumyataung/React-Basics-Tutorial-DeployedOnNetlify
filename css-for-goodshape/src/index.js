eea42ae2828e0ec8c18e9ad76672b4652463114b
Thiri Hsu Myat  Aung <thirihsumyataung@thiris-imac.myfiosgateway.com>
2021-05-05 18:25:40 -0400

    css

---
+++ css-for-goodshape/src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+//import Book from './components/Booklist.js';
+
+//CSS
+
+import './index.css'; 
+function BookList() {
+    return(
+        <section className="bookList">
+            <Book />          
+        </section>
+    )
+}
+ const Book = () => {
+     return (<article className="book">
+         <Images />
+         <Title />
+         <Author/>
+     </article>); 
+}    
+
+const Images =() => <img src="https://m.media-amazon.com/images/I/51DNaiWfllL.jpg" alt =""/>
+
+const Title = () => <h1>The Beach House</h1>
+
+const Author = () => <h5>by Rachel Hanna</h5>
+
+ReactDom.render(<BookList/>, document.getElementById('root'));
